65cb5529c12bffcd4f3a8c5c57025a66edef5b5e
Temirlan <zatovagul@gmail.com>
2022-10-14 17:24:40 +0300

    Q4: Widget test configuration

--- test/test_setup.dart
+++ test/test_setup.dart
@@ -1,13 +1,12 @@
 import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:injectable/injectable.dart';
 import 'package:qyre_test_app/di/di.dart';
 
 void testSetUp() {
   setUpAll(() async {
-    WidgetsFlutterBinding.ensureInitialized();
+    TestWidgetsFlutterBinding.ensureInitialized();
     configureDependencies(const Environment(Environment.test));
     HttpOverrides.global = null;
   });
